0d91407ffd20a0a1bc2698fe8808f3789b86fe45
rebecagode <106184156+rebecagode@users.noreply.github.com>
2022-10-26 16:52:43 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -66,9 +66,9 @@ function total(){
    for(let m = 1; m <= t; m++){
       r = (val * (1+ (ju/100)));
       val = r;
-      document.write("Mês " + m + " valor: " + moeda(r) + "<br>");
+     // document.write("Mês " + m + " valor: " + moeda(r) + "<br>");
    }
+   document.getElementById("total").innerHTML = "total: " +moeda (r);
    
-   document.write("O total é " + moeda(r));
+  // document.write("O total é " + moeda(r));
 }
-
